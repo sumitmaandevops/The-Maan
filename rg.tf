d4b277c4d970b99b12755541a889dce30d50ae3f
Sumitmaandevops <sumitkumar69d@gmail.com>
2025-08-09 23:37:26 +0530

    new rg Added

--- rg.tf
+++ rg.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "rg1" {
   name     = "sumitrg"
   location = "central india"
 }
+resource "azurerm_resource_group" "rg2" {
+  name     = "sumitrg1"
+  location = "central india"
+}
