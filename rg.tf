274291ab4c0e85fd839f824ebe0d4f678249909b
Sumitmaandevops <sumitkumar69d@gmail.com>
2025-08-09 23:52:32 +0530

    rg.tf deleted

--- rg.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_resource_group" "rg1" {
-  name     = "sumitrg"
-  location = "central india"
-}
-resource "azurerm_resource_group" "rg2" {
-  name     = "sumitrg1"
-  location = "central india"
-}
